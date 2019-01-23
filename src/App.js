f5310999a956383abdabe8935da01d533eb9b6b1
Alexey Levchenko <chipsa532@gmail.com>
2019-01-23 20:40:46 +0200

    scroll to top after redirect

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import * as screens from 'screens';
 
+const ScrollToTop = () => {
+  window.scrollTo(0, 0);
+  return null;
+};
+
 const App = () => (
 	<div>
+    <Route component={ScrollToTop} />
     <Switch>
       <Route path="/log-in" component={screens.LogInScreen} />
       <Route path="/auth" component={screens.AuthorizationScreen} />
